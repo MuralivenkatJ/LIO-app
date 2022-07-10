cf275d6af936b3dd97e8981277656f7aeb82e101
MAHENDRA S M <mahendrasmmahesh@gmail.com>
2022-07-10 14:48:01 +0530

    updated

--- XML Kotlin Frontend/app/src/main/java/com/example/lio/Interfaces/Faculty.kt
+++ XML Kotlin Frontend/app/src/main/java/com/example/lio/Interfaces/Faculty.kt
@@ -1,4 +1,27 @@
 package com.example.lio.Interfaces
 
+import com.example.facultyregister.Models.GetInstitutes
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.*
+
+
 interface Faculty {
+
+ @GET("getInstitutes")
+    fun getInstitutes(): Call<List<GetInstitutes>>
+
+    //@Multipart
+    @Multipart
+    @POST("faculty/register")
+    fun registerFaculty(
+        @Part("name") n:RequestBody,
+        @Part("email") e:RequestBody,
+        @Part("password") p:RequestBody,
+        @Part("qualification") q:RequestBody,
+        @Part("phone") ph:RequestBody,
+        @Part("institute") i:RequestBody,
+        @Part image: MultipartBody.Part
+    ): Call<String>
 }
